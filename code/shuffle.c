3beb4fbb97a6180b948fd9624f57cf5e6be8cf4b
garmobal <garmobal@gmail.com>
2022-08-16 17:28:20 +0200

    upgrade to v2.1.2

--- code/shuffle.c
+++ code/shuffle.c
@@ -1,5 +1,6 @@
-// Define a function called `shuffle` that
-// receives an array of integers (int[]) and shuffles the elements
-int main(int argc, char **argv) {
+// Define a function called `shuffle`
+// that receives an array of integers (int[])
+// and shuffles the elements
+int main (int argc, char **argv) {
   return 0;
 }
