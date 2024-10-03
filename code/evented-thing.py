e04e2b11d797f6aeeb05390b15c5302a12d283ed
CW Self Paced Student <testselfpaced@gmail.com>
2024-10-03 12:35:52 +0000

    all done

--- code/evented-thing.py
+++ code/evented-thing.py
@@ -11,3 +11,56 @@
 #
 # eventedThing.trigger('whatever');
 # -> nothing happens
+
+import inspect
+
+class EventedThing:
+    def __init__(self):
+        self.store = {}
+
+    def on(self, name, handler):
+        if name not in self.store:
+            self.store[name] = []
+        self.store[name].append(handler)
+    
+    def trigger(self, name, *args, **kwargs):
+        if name in self.store:
+            for handler in self.store[name]:
+                sig = inspect.signature(handler)
+                params = sig.parameters
+                if len(params) == 0:
+                    handler()
+                else:
+                    handler(*args, **kwargs)
+        else:
+            pass  # Do nothing if the event is not registered
+
+
+eventedThing = EventedThing()
+
+# Define handlers
+def say_hi():
+    print('Hi there!')
+
+def greet(name):
+    print('Nice to meet you, ' + name + '.')
+
+
+# Register handlers
+eventedThing.on('meet', say_hi)
+eventedThing.on('meet', greet)
+
+# Trigger the 'meet' event
+eventedThing.trigger('meet', 'Sarah')
+
+# Output:
+# Hi there!
+# Nice to meet you, Sarah.
+
+# Trigger an unregistered event
+eventedThing.trigger('whatever')
+# Output:
+# (Nothing happens)
+
+
+
